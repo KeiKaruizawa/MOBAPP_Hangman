e8b0d4ac951bbbfc870e8c2666de7bd7a7a8c665
arya0250 <jabolasco@up.edu.ph>
2025-10-27 01:32:17 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Hangman
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PlayTapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Game());
+        }
+
+
+    }
+
+}
